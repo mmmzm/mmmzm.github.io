65c1ebf51e9929e5e27ff5ab2d682910958aa0b4
박현아 <167169191+mmmzm@users.noreply.github.com>
2024-09-08 17:59:58 +0900

    포토폴리오 업데이트

---
+++ index.js
@@ -0,0 +1,41 @@
+// 맨 위로 이동 버튼
+
+let moveToTop = function () {
+  document.body.scrollIntoView({ behavior: "smooth" });
+};
+
+// 모달
+const $modalBg = document.getElementsByClassName("modal-background");
+const $btnOpen = document.getElementsByClassName("btn-open");
+const $btnClose = document.getElementsByClassName("btn-close");
+
+function modal(num) {
+  $btnOpen[num].addEventListener("click", () => {
+    $modalBg[num].style.display = "flex";
+    document.body.style.overflow = "hidden";
+  });
+  $btnClose[num].addEventListener("click", () => {
+    $modalBg[num].style.display = "none";
+    document.body.style.overflow = "unset";
+  });
+}
+
+for (let i = 0; i < $btnOpen.length; i++) {
+  modal(i);
+}
+
+// 스크롤바
+let scrollTop = 0;
+let bar = document.getElementsByClassName("bar-ing")[0];
+
+window.addEventListener(
+  "scroll",
+  () => {
+    scrollTop = document.documentElement.scrollTop;
+    let per = Math.ceil(
+      (scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100
+    );
+    bar.style.width = per + "%";
+  },
+  false
+);
